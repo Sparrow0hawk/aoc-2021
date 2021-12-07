9ee97813f341743533ca296db8b5d3e6a5c5b547
Sparrow0hawk <alexjcoleman@gmail.com>
2021-12-07 20:27:35 +0000

    adding main.py
    entrypoint into python solutions

---
+++ python/main.py
@@ -0,0 +1,22 @@
+import sys
+import importlib
+
+
+def main(day: str):
+
+    challenge_cls = getattr(importlib.import_module(day), "Challenge")
+
+    challenge_inst = challenge_cls(day=day)
+
+    challenge_inst.get_data()
+
+    challenge_inst.get_result()
+
+    return challenge_inst.return_result()
+
+
+if __name__ == "__main__":
+
+    day = sys.argv[1]
+
+    print(main(day))
